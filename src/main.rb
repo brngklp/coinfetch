b5eba1e5955455835fb2b7286a21eb00e8be53dc
Baran Gökalp <61322347+brngklp@users.noreply.github.com>
2021-10-20 19:06:58 +0300

    Create main.rb

---
+++ src/main.rb
@@ -0,0 +1,24 @@
+require "httparty"
+require "colorize"
+
+# Argument Parsing
+args = Hash[ ARGV.join(' ').scan(/--?([^=\s]+)(?:=(\S+))?/) ]
+
+symbol = args["s"]
+currency = args["c"]
+
+# Request
+host = "https://api.coinbase.com/v2/prices/#{symbol}-#{currency}/spot"
+response = HTTParty.get("#{host}")
+parsed_response = response.dig('data', 'amount')
+
+# Print it
+puts("""     
+──▄▄█▀▀▀▀▀█▄▄──
+▄█▀░░▄░▄░░░░▀█▄     
+█░░░▀█▀▀▀▀▄░░░█     
+█░░░░█▄▄▄▄▀░░░█     Price(#{symbol.upcase} to #{currency.upcase}): #{parsed_response}
+█░░░░█░░░░█░░░█
+▀█▄░▀▀█▀█▀░░▄█▀
+──▀▀█▄▄▄▄▄█▀▀──
+    """.yellow)
